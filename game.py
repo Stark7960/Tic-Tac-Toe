1e52762f4183f2489f262bac9dd4f337dff62220
Pratyaksh Jindal <pratyakshjindal99@gmail.com>
2022-06-19 04:42:04 +0530

    commit

--- game.py
+++ game.py
@@ -1,14 +1,3 @@
-"""
-Tic Tac Toe class + game play implementation by Kylie Ying
-YouTube Kylie Ying: https://www.youtube.com/ycubed 
-Twitch KylieYing: https://www.twitch.tv/kylieying 
-Twitter @kylieyying: https://twitter.com/kylieyying 
-Instagram @kylieyying: https://www.instagram.com/kylieyying/ 
-Website: https://www.kylieying.com
-Github: https://www.github.com/kying18 
-Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
-"""
-
 import math
 import time
 from player import HumanPlayer, RandomComputerPlayer, SmartComputerPlayer
@@ -43,24 +32,24 @@ class TicTacToe():
         return False
 
     def winner(self, square, letter):
-        # check the row
+        
         row_ind = math.floor(square / 3)
         row = self.board[row_ind*3:(row_ind+1)*3]
-        # print('row', row)
+        
         if all([s == letter for s in row]):
             return True
         col_ind = square % 3
         column = [self.board[col_ind+i*3] for i in range(3)]
-        # print('col', column)
+        
         if all([s == letter for s in column]):
             return True
         if square % 2 == 0:
             diagonal1 = [self.board[i] for i in [0, 4, 8]]
-            # print('diag1', diagonal1)
+            
             if all([s == letter for s in diagonal1]):
                 return True
             diagonal2 = [self.board[i] for i in [2, 4, 6]]
-            # print('diag2', diagonal2)
+           
             if all([s == letter for s in diagonal2]):
                 return True
         return False
@@ -96,8 +85,8 @@ def play(game, x_player, o_player, print_game=True):
             if game.current_winner:
                 if print_game:
                     print(letter + ' wins!')
-                return letter  # ends the loop and exits the game
-            letter = 'O' if letter == 'X' else 'X'  # switches player
+                return letter 
+            letter = 'O' if letter == 'X' else 'X'  
 
         time.sleep(.8)
 
--- player.py
+++ player.py
@@ -1,13 +1,4 @@
-"""
-Tic-Tac-Toe players using inheritance implementation by Kylie YIng
-YouTube Kylie Ying: https://www.youtube.com/ycubed 
-Twitch KylieYing: https://www.twitch.tv/kylieying 
-Twitter @kylieyying: https://twitter.com/kylieyying 
-Instagram @kylieyying: https://www.instagram.com/kylieyying/ 
-Website: https://www.kylieying.com
-Github: https://www.github.com/kying18 
-Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
-"""
+
 
 import math
 import random
@@ -61,10 +52,9 @@ class SmartComputerPlayer(Player):
         return square
 
     def minimax(self, state, player):
-        max_player = self.letter  # yourself
+        max_player = self.letter  
         other_player = 'O' if player == 'X' else 'X'
 
-        # first we want to check if the previous move is a winner
         if state.current_winner == other_player:
             return {'position': None, 'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (
                         state.num_empty_squares() + 1)}
@@ -72,19 +62,18 @@ class SmartComputerPlayer(Player):
             return {'position': None, 'score': 0}
 
         if player == max_player:
-            best = {'position': None, 'score': -math.inf}  # each score should maximize
+            best = {'position': None, 'score': -math.inf}  
         else:
-            best = {'position': None, 'score': math.inf}  # each score should minimize
+            best = {'position': None, 'score': math.inf}  
         for possible_move in state.available_moves():
             state.make_move(possible_move, player)
-            sim_score = self.minimax(state, other_player)  # simulate a game after making that move
+            sim_score = self.minimax(state, other_player)  
 
-            # undo move
             state.board[possible_move] = ' '
             state.current_winner = None
-            sim_score['position'] = possible_move  # this represents the move optimal next move
+            sim_score['position'] = possible_move 
 
-            if player == max_player:  # X is max player
+            if player == max_player:  
                 if sim_score['score'] > best['score']:
                     best = sim_score
             else:
